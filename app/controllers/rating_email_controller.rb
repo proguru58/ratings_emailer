3df765f505424bf01793a83f32269389338dbeb4
Daniel Gabris <softabroad71@outlook.com>
2016-11-14 18:21:40 +0100

    fix

--- app/controllers/rating_email_controller.rb
+++ app/controllers/rating_email_controller.rb
@@ -6,7 +6,7 @@ class RatingEmailController < ApplicationController
     content = params[:rating_email][:html]
     RatingsMailer.send_ratings(current_user.email, params[:rating_email][:emails], content).deliver
 
-    flash[:notice] = '5 Stars rating email sent'
+    flash[:notice] = 'Email sent with 5 stars rating attached'
     redirect_to rating_email_index_path
   end
 end
