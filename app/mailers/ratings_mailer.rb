8e5f53b3de3ce032e19e4fee42ebe9501561386d
Daniel Gabris <softabroad71@outlook.com>
2016-11-15 10:41:19 +0100

    added links to user

--- app/mailers/ratings_mailer.rb
+++ app/mailers/ratings_mailer.rb
@@ -1,6 +1,8 @@
 class RatingsMailer < ApplicationMailer
-  def send_ratings(from_address, to_addresses, content)
+  def send_ratings(from_address, to_addresses, content, positive_rating_link, negative_rating_link)
     @content = content
+    @positive_rating_link = positive_rating_link
+    @negative_rating_link = negative_rating_link
     mail(:from => from_address, :to => to_addresses, :subject=>"Hotel Ratings")
   end
 end
