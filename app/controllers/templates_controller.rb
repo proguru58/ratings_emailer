b48e89afc44d70ea403b3bc7b74c0de45afe1bb1
Daniel Gabris <softabroad71@outlook.com>
2016-11-29 04:10:03 +0100

    templates crud

---
+++ app/controllers/templates_controller.rb
@@ -0,0 +1,64 @@
+class TemplatesController < ApplicationController
+  before_action :find_template, only: [:edit, :update, :show, :delete]
+
+  # Index action to render all templates
+  def index
+    @templates = Template.all
+  end
+
+  # New action for creating template
+  def new
+    @template = Template.new
+  end
+
+  # Create action saves the template into database
+  def create
+    @template = Template.new
+    if @template.save(template_params)
+      flash[:notice] = "Successfully created template!"
+      redirect_to template_path(@template)
+    else
+      flash[:alert] = "Error creating new template!"
+      render :new
+    end
+  end
+
+  # Edit action retrives the template and renders the edit page
+  def edit
+  end
+
+  # Update action updates the template with the new information
+  def update
+    if @template.update_attributes(template_params)
+      flash[:notice] = "Successfully updated template!"
+      redirect_to template_path(@templates)
+    else
+      flash[:alert] = "Error updating template!"
+      render :edit
+    end
+  end
+
+  # The show action renders the individual template after retrieving the the id
+  def show
+  end
+
+  # The destroy action removes the template permanently from the database
+  def destroy
+    if @template.destroy
+      flash[:notice] = "Successfully deleted template!"
+      redirect_to templates_path
+    else
+      flash[:alert] = "Error updating template!"
+    end
+  end
+
+  private
+
+  def template_params
+    params.require(:template).permit(:title, :body)
+  end
+
+  def find_template
+    @template = Template.find(params[:id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115084124) do
+ActiveRecord::Schema.define(version: 20161129025705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "templates", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
