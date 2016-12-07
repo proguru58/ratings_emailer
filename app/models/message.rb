f966a54e1259e7ec2f90e8a5cbb05646de0004bd
Daniel Gabris <softabroad71@outlook.com>
2016-12-07 03:30:46 +0100

    fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,8 +3,6 @@ class Message < ApplicationRecord
   belongs_to :user
 
   def mark_as_read
-    if self.read_at.nil?
-      self.update_column(:status, 'opened')
-    end
+    self.update_column(:status, 'opened')
   end
 end
