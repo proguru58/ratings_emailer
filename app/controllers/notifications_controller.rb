0519fdc41b6843fb332b09479dc1a8df0b2fc9dd
Daniel Gabris <softabroad71@outlook.com>
2016-12-07 04:00:48 +0100

    fix

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,13 +5,9 @@ class NotificationsController < ApplicationController
     @message = Message.find_by_token(params[:message_token_id])
     @message.mark_as_read unless @message.nil?
 
-    filename = params[:filename].gsub(/[^a-zA-Z0-9_]/,'')
-
     respond_to do |format|
       begin
-        file_path = "../../../assets/images/#{filename}.#{params[:format]}"
-        file = File.open(File.expand_path(file_path, __FILE__))
-        format.gif { render :text => file.read }
+        format.gif { render :text => 'Here' }
       rescue
         format.any { render :template => 'application/errors/404', :status => :not_found }
       end
